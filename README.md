592854349d678ee2ea45fb2b0b59c21f91d491c2
韩兵 <hanbingango@didichuxing.com>
2016-11-11 21:24:59 +0800

    webpack plugin

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # assets-json-webpack-plugin
 output assets json
+
+```javascript
+const AssetsJSONPlugin = require('assets-json-webpack-plugin');
+module.exports = {
+  ...
+  plugins: [
+    new AssetsJSONPlugin({output: path.resolve(__dirname, './public')})
+  ]
+}
+```
---
+++ index.js
@@ -0,0 +1,21 @@
+var fs = require('fs-extra');
+var path = require('path');
+
+module.exports = function(config){
+  var output = config.output;
+  return function() {
+    this.plugin('done', function (stats) {
+      const {
+        hash,
+        assetsByChunkName: assets,
+        publicPath
+      } = stats.toJson();
+      if(!output) output = this.outputPath;
+      fs.mkdirsSync(output);
+      fs.writeFileSync(
+        path.resolve(output, './assets.json'),
+        JSON.stringify({ hash, assets }, null, 2)
+      );
+    });
+  }
+};
